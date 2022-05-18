9ad0ed5ee0ecfd9918faf62f682a6769e18082dd
ercheung3 <ercheung3@gmail.com>
2022-05-18 16:43:11 -0400

    Added navbar links and styling

--- portfolio-react/src/components/Sidebar/index.js
+++ portfolio-react/src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -9,6 +11,36 @@ const Sidebar = () => (
       <img src={LogoS} alt="logo" />
       <img className="sub-logo" src={LogoSubtitle} alt="eric" />
     </Link>
+
+    <nav>
+      <NavLink
+        className="home-link"
+        exact="true"
+        activeclassname="active"
+        to="/"
+      >
+        {/*CHANGE THE ICON COLOR HERE*/}
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        className="about-link"
+        exact="true"
+        activeclassname="active"
+        to="/about"
+      >
+        {/*CHANGE THE ICON COLOR HERE*/}
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        className="contact-link"
+        exact="true"
+        activeclassname="active"
+        to="/contact"
+      >
+        {/*CHANGE THE ICON COLOR HERE*/}
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
   </div>
 )
 
