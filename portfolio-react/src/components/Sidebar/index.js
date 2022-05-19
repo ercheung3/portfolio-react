c70cfb0333fb4fa1d400dc9eab72675c9c58d772
ercheung3 <ercheung3@gmail.com>
2022-05-19 12:35:01 -0400

    Added social links

--- portfolio-react/src/components/Sidebar/index.js
+++ portfolio-react/src/components/Sidebar/index.js
@@ -3,8 +3,13 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faSquare,
+} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
@@ -41,6 +46,30 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/ercheung3/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d42" />
+        </a>
+      </li>
+
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/ercheung3">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d42" />
+        </a>
+      </li>
+
+      <li>
+        <a target="_blank" rel="noreferrer" href="#">
+          <FontAwesomeIcon icon={faSquare} color="#4d4d42" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
